b62ee6b369f1969878e8b297e229e6a9bc6684ad
Patrick Bellasi <patrick.bellasi@arm.com>
2016-08-24 11:27:27 +0100

    FIXUP: sched/tune: update accouting before CPU capacity
    The SchedTune tasks accounting is used to identify how many tasks are in
    a boostgroup and thus to bias the selection of an OPP based on the
    maximum boost value of the active boostgroups.
    The current implementation however update the accounting after CPU
    capacity has been update. This has two effects:
    a) when we enqueue a boosted task, we do not immediately boost its CPU
    b) when we dequeue a boosted task, we can keep a CPU boosted even if not
       required
    
    This patch change the order of the SchedTune accounting and SchedFreq
    updated to ensure to have always an updated representation of which
    boosted tasks are runnable on a CPU before updating its capacity.
    
    Change-Id: I078ef59c6e8f9cb15a9fb4e0b26fe01f1bc5fed9
    Reported-by: Leo Yan <leo.yan@linaro.org>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    [ minor merge changes ]
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4504,9 +4504,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 			update_capacity_of(cpu_of(rq));
 	}
 
-	/* Update SchedTune accouting */
-	schedtune_enqueue_task(p, cpu_of(rq));
-
 #endif /* CONFIG_SMP */
 
 	hrtick_update(rq);
@@ -4600,9 +4597,6 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 				set_cfs_cpu_capacity(cpu_of(rq), false, 0);
 		}
 	}
-
-	/* Update SchedTune accouting */
-	schedtune_dequeue_task(p, cpu_of(rq));
 
 #endif /* CONFIG_SMP */
 
